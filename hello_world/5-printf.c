02e94aa7e420c5ce20fd4f14af022aef6029464a
Alex <9761@holbertonstudents.com>
2024-10-21 13:14:14 +0200

    Ajout du programme 5 non compilé et supression de la version compilée

---
+++ hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the text "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
