aa4a690206e019010c7502cc168c42adf47fe632
Alex <9761@holbertonstudents.com>
2024-10-22 10:21:25 +0200

    Ajout du programme 2

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: This program print the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char c;
+
+
+for (c = 'a'; c <= 'z'; c++)
+{
+	putchar(c);
+}
+	putchar('\n');
+
+	return (0);
+}
