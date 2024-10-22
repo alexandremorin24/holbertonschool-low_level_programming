e5254c312cbabd9cdeb949b55064977fa28b8241
Alex <9761@holbertonstudents.com>
2024-10-22 11:18:17 +0200

    Ajout du programme 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: This program print the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int numberz = '0';
+
+
+for (numberz = '0'; numberz <= '9'; numberz++)
+{
+	putchar(numberz);
+}
+	putchar('\n');
+
+	return (0);
+}
