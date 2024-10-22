c32fbd9fb3e48c99dde23e7882ab292ffabaab86
Alex <9761@holbertonstudents.com>
2024-10-22 11:10:29 +0200

    Ajout du programme 5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry point
+*
+* Description: This program print the alphabet in lowercase
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf("0123456789");
+
+return (0);
+}
