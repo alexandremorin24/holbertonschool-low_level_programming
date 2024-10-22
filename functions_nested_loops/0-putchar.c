7121fabe318b601e68a2d3ea07e72994271563b9
Alex <9761@holbertonstudents.com>
2024-10-22 15:35:16 +0200

    Ajout de main.h et du premier programme

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,6 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <main.h>
 /**
 * main - Entry point
 *
@@ -11,5 +9,13 @@
 */
 int main(void)
 {
-	printf("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
 }
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H  // Eviter inclusion multiple
+#define MAIN_H
+
+int _putchar(char c);  // Prototype de _putchar
+
+#endif
