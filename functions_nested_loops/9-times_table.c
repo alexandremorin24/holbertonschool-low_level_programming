286e394a488ef2a331ad583ebee59950b30e84d3
Alex <9761@holbertonstudents.com>
2024-10-23 15:39:05 +0200

    Ajout du programme 9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ * Return: Void
+ */
+void times_table(void)
+{
+    int firstNumber, secondNumber, result;
+
+    for (firstNumber = 0; firstNumber <= 9; firstNumber++)
+    {
+        for (secondNumber = 0; secondNumber <= 9; secondNumber++)
+        {
+            result = firstNumber * secondNumber;
+
+            if (result < 10)
+	    {
+                if (secondNumber != 0)
+                {
+                    _putchar(' ');
+                }
+                _putchar(result + '0');
+            }
+            else
+            {
+                _putchar((result / 10) + '0');
+                _putchar((result % 10) + '0');
+            }
+
+            if (secondNumber != 9)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+        }
+        _putchar('\n');
+    }
+}
+
