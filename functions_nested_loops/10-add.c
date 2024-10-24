174194313426e817a1f8f34f1c67e20714c1a227
Alex <9761@holbertonstudents.com>
2024-10-24 11:40:26 +0200

    Ajour du programme 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * addition - adds two integers and returns the result
+ *@num1: first number
+ *@num2: second number
+ * Return: result
+ */
+int addition(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
