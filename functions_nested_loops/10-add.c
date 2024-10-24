b36a8dd9d0b2f64e1a1b3c5de9691c16c6034e11
Alex <9761@holbertonstudents.com>
2024-10-24 14:12:07 +0200

    Ajout du programme 9 remplacé par erreur plus tôt

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,39 +1,14 @@
 #include "main.h"
 
 /**
- * print_to_98 - prints all natural numbers from n to 98
- *
- *@n: starting number
- *
- * Return: result
- */
-void print_to_98(int n)
+* add - adds two integers and returns the result
+* @num1: first number
+* @num2: second number
+*
+* Return: result of addition
+*/
+int add(int num1, int num2)
 {
-	int number;
-
-	if (n < 98)
-	{
-		for (number = n; number <= 98; number++)
-		{
-			printf("%d", number);
-
-			if (number != 98)
-			{
-				printf(", ");
-			}	
-		}
-	}
-	else
-	{
-		for (number = n; number >= 98; number--)
-		{
-			printf("%d", number);
-
-			if (number != 98)
-			{
-				printf(", ");
-			}
-		}
-	}
-	printf("\n");
+	int result = num1 + num2;
+	return (result);
 }
