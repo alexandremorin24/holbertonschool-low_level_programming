4223fd9b72d87fa7af107bfcec6d98daae8183cd
Alex <9761@holbertonstudents.com>
2024-10-28 09:55:14 +0100

    Ajout du programme 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * void print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
+ * Return: Void
+ */
+void print_numbers(void)
+{
+	for(int i = 0; i < 10; i++)
+	{
+		_putchar( i + '0');
+	}
+	_putchar('\n');
+}
