c6edfc2a34374b18b63057b1c30de04c27e0ed7f
Alex <9761@holbertonstudents.com>
2024-10-28 11:26:39 +0100

    Ajout du programme 8

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square of n blocs
+ *
+ * @n: number of blocs
+ *
+ * Return: Void
+ */
+
+void print_square(int size)
+{
+	int i = 0, j = 0;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar ('#');
+			}
+
+			_putchar ('\n');
+
+		}
+	}
+	if (size <= 0)
+	{
+		_putchar ('\n');
+
+	}
+}
