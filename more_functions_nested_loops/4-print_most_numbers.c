cff732b95f309bb2152330a65d64da156de60f0f
Alex <9761@holbertonstudents.com>
2024-10-28 10:04:17 +0100

    Ajout du programme 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers from 0 to 9 without 2 and 4
+ * followed by a new line
+ *
+ * Return: Void
+ */
+void print_more_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i!=4)
+		{
+			_putchar (i + '0');
+		}i
+	}
+	_putchar('\n');
+}
