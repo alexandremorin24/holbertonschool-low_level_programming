073f0e06682ebea13f92922c9163a411ddae3d3f
Alex <9761@holbertonstudents.com>
2024-10-29 10:44:52 +0100

    Ajout du programme 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * int _strlen - returns the length of a string
+ *
+ * @s: string to mesure
+ *
+ * Return: lenght of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	return length;
+}
