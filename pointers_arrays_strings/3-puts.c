bc307391b8842eea2acef0ad286d8080ae9248c9
Alex <9761@holbertonstudents.com>
2024-10-29 11:17:13 +0100

    Ajout du programme 3

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *  _puts - prints a string, followed by a new line, to stdout
+ *
+ * @s: string to mesure
+ *
+ * Return: lenght of the string
+ */
+void _puts(char *str)
+{
+	int i = 0; 
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
