11b57ed4084d4ca699256141ec077f987451d188
Alex <9761@holbertonstudents.com>
2024-10-29 13:19:06 +0100

    Modification du programme 4 pour ne pas utiliser _strlen

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,12 @@
  */
 void print_rev(char *s)
 {
-	int i = _strlen(s) - 1;
+	int i = 0;
 
-	for (; s[i] >= 0; i--)
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
