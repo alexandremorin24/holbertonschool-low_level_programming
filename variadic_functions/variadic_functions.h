b8ee13759a51e4f3cfb35c7f24818bb4cc437491
Alex <9761@holbertonstudents.com>
2024-11-20 15:45:07 +0100

    Ajout du programme 3-print_all.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+/**
+ * struct printer - associates a type with a print function
+ * @type: the type of argument (e.g., 'c', 'i', 'f', 's')
+ * @func: pointer to the function that prints the type
+ */
+
+typedef struct printer
+{
+	char type;
+	void (*func)(va_list args, char *separator);
+} printer_t;
+
 #endif
