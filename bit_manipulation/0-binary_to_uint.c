c9e8b3b793be6354f2a80995b4e17c0d15280952
Alexandre <9761@holbertonstudents.com>
2024-12-06 15:19:37 +0100

    modification de la task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -19,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b == NULL || b[i] != '0' && b[i] != '0')
+		if (b == NULL || (b[i] != '0' && b[i] != '0'))
 			return (0);
 
 		result += result * 2 + (b[i] - '0');
