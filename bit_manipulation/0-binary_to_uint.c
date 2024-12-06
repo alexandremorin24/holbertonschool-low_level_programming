b1d9bb35016d764e03cea2dcbde2366b6f51984d
Alexandre <9761@holbertonstudents.com>
2024-12-06 15:21:43 +0100

    Correction de la vérification et du calcul

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -19,10 +19,10 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b == NULL || (b[i] != '0' && b[i] != '0'))
+		if (b == NULL || (b[i] != '0' && b[i] != '1'))
 			return (0);
 
-		result += result * 2 + (b[i] - '0');
+		result = result * 2 + (b[i] - '0');
 	}
 	return (result);
 }
